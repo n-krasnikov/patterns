f1dd89bb1de8186060ec097de7a7749b92bcf8f5
Krasnilov Nikita <n.krasnikov@tg.dunice.net>
2023-10-03 16:51:45 +0300

    (examples) Observer

--- main.js
+++ main.js
@@ -25,6 +25,27 @@ console.log(Module.square(5)); // Public in return
 
 /*----------------------------*/
 
+let count = 0;
+let slowCount = 0;
+  
+const Obs = new Observer();
+
+const incCount = () => count++;
+const slowIncCount = () => setTimeout(() => slowCount++, 5000);
+
+const get = () => console.log(count);
+const getSlow = () => console.log(slowCount);
+
+Obs.subscribe(incCount);
+Obs.subscribe(slowIncCount);
+
+Obs.subscribe(get);
+Obs.subscribe(getSlow);
+
+document.addEventListener('click', () => Obs.notify());
+
+/*-----------------------------*/
+
 document.querySelector('#app').innerHTML = `
   <div>
     <h1>Design Patterns</h1>
--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 export {default as Singleton} from './Singleton';
 export {default as Module} from './Module';
+export {default as Observer} from './Observer';
